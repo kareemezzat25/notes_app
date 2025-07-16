fd756fe4643135f86197d9e2d4ef95aef10523e6
kareemezzat <kareemezzat900@gmail.com>
2025-07-17 02:47:16 +0300

    Change appLogo & Name

--- lib/widgets/editnote_body.dart
+++ lib/widgets/editnote_body.dart
@@ -13,7 +13,7 @@ class EditNoteBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16),
       child: Column(
         children: [
           CustomTextFormField(labelText: "Title", controller: titleController),
